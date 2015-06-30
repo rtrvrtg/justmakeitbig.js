d6b1c93fc78cbc9e58edf41a06c8c65251458a0e
Geoffrey Roberts <g.roberts@blackicemedia.com>
2015-06-30 22:31:07 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# JustMakeItBig.js
+
+A quick and dirty layer for HTML5 Fullscreen API, with fallback for browsers that don't support it
+
+## Authorship
+
+Written by [Geoffrey Roberts](mailto:g.roberts@blackicemedia.com)
+
+Faux fullscreen fallback inspired by, but not a direct implementation of, the fullscreen handling in [OpenSeadragon](https://openseadragon.github.io/).
+
+## License
+
+MIT
+
+@TODO: Set up a LICENSE file
+
+## Requirements
+
+* jQuery
+
+## Installation
+
+In the `<head>` of your page, after you set up your jQuery `<script>` item, add the following:
+
+```html
+<script type="text/javascript" src="justmakeitbig.js"></script>
+```
+
+## Usage
+
+### JustMakeItBig.canFullscreen
+
+Takes no parameters.
+
+Returns `TRUE` if it supports either HTML5 fullscreen API or fallback, `FALSE` otherwise.
+
+### JustMakeItBig.toggle
+
+Takes two parameters:
+
+* `elem` - the element to display fullscreen
+* `toggleElem` - the element we clicked to toggle fullscreen display
+
+Returns nothing.
+
+## Changelog
+
+### v0.1
+
+Initial commit
---
+++ justmakeitbig.js
@@ -0,0 +1,157 @@
+
+
+var JustMakeItBig = {
+  // HTML5 Fullscreen API
+  _fullscreen: {
+    canFullScreen: function(){
+      return (
+        !!document.exitFullscreen ||
+        !!document.msExitFullscreen ||
+        !!document.webkitExitFullscreen ||
+        !!document.webkitCancelFullScreen ||
+        !!document.mozCancelFullScreen
+      );
+    },
+    isFullScreen: function(){
+      return (
+        !!document.fullscreenElement ||
+        !!document.msFullscreenElement ||
+        !!document.mozFullScreenElement ||
+        !!document.webkitFullscreenElement ||
+        !!document.webkitCurrentFullScreenElement
+      );
+    },
+    enterFullScreen: function(elem, whenToggled){
+      if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+      } else if (elem.msRequestFullscreen) {
+        elem.msRequestFullscreen();
+      } else if (elem.mozRequestFullScreen) {
+        elem.mozRequestFullScreen();
+      } else if (elem.webkitRequestFullscreen) {
+        elem.webkitRequestFullscreen();
+      } else if (elem.webkitRequestFullScreen) {
+        elem.webkitRequestFullScreen();
+      }
+      whenToggled(true);
+    },
+    exitFullScreen: function(whenToggled){
+      if(document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if(document.msExitFullscreen) {
+        document.msExitFullscreen();
+      } else if(document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if(document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      } else if(document.webkitCancelFullScreen) {
+        document.webkitCancelFullScreen();
+      }
+      whenToggled(false);
+    }
+  },
+
+  // Psuedo-fullscreen for browsers that can't do fullscreen normally
+  // eg. iOS Safari
+  _fullpage: {
+    _elemStyle: {
+    },
+    _pageScroll: null,
+    _keyupAction: function(e){
+      // ESC key to close full page
+      if (e.which == 27) {
+        _fullpage.exitFullPage();
+      }
+    },
+    _attachEvents: function(){
+      $(document).bind('keyup', JustMakeItBig.fullpage._keyupAction);
+    },
+    _detachEvents: function(){
+      $(document).unbind('keyup', JustMakeItBig.fullpage._keyupAction);
+    },
+    canFullPage: function(){
+      return true;
+    },
+    isFullPage: function(){
+      return $(document).find('[data-justmakeitbig-full-page-elem]').length > 0;
+    },
+    enterFullPage: function(elem, toggleElement, whenToggled) {
+      $('body').css('overflow', 'hidden');
+      $(elem).attr(
+        'data-justmakeitbig-full-page-elem',
+        '0'
+      ).data(
+        'JustMakeItBig.stashScroll',
+        $(document).scrollTop()
+      ).data('JustMakeItBig.stashStyle', {
+        position: $(elem).css('position'),
+        width: $(elem).css('width'),
+        height: $(elem).css('height'),
+        top: $(elem).css('top'),
+        left: $(elem).css('left'),
+        zIndex: $(elem).css('z-index')
+      }).css({
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: $(window).innerWidth(),
+        height: $(window).innerHeight(),
+        zIndex: 999999
+      }).attr('data-justmakeitbig-full-page-elem', "1");
+      toggleElement.focus();
+      JustMakeItBig._fullpage._attachEvents();
+      $(document).scrollTop(0);
+      whenToggled(true);
+    },
+    exitFullPage: function(toggleElement){
+      JustMakeItBig._fullpage._detachEvents();
+      $(document).find('[data-justmakeitbig-full-page-elem]').each(function(){
+        $(this).css(
+          $(this).data('JustMakeItBig.stashStyle')
+        ).removeAttr('data-justmakeitbig-full-page-elem');
+      });
+      $('body').css('overflow', 'auto');
+      toggleElement.focus();
+      $(document).scrollTop();
+      whenToggled(false);
+    }
+  },
+  // API function: can display this in full screen
+  canFullscreen: function(){
+    return JustMakeItBig._fullscreen.canFullScreen() || JustMakeItBig._fullpage.canFullPage();
+  },
+  toggle: function(elem, toggleElement, whenToggled){
+    if (!whenToggled) {
+      whenToggled = function(){};
+    }
+    if (!!elem) {
+      if (JustMakeItBig._fullscreen.canFullScreen()) {
+        if (JustMakeItBig._fullscreen.isFullScreen()) {
+          JustMakeItBig._fullscreen.exitFullScreen(whenToggled);
+        }
+        else {
+          JustMakeItBig._fullscreen.enterFullScreen(
+            elem,
+            whenToggled
+          );
+        }
+      }
+      else if (JustMakeItBig._fullpage.canFullPage()) {
+        if (JustMakeItBig._fullpage.isFullPage()) {
+          JustMakeItBig._fullpage.exitFullPage(
+            elem,
+            toggleElement,
+            whenToggled
+          );
+        }
+        else {
+          JustMakeItBig._fullpage.enterFullPage(
+            elem,
+            toggleElement,
+            whenToggled
+          );
+        }
+      }
+    }
+  }
+};
